3e8f0edcda51dfcc6bf191f25dc8b4d2a2e13794
wolframteetz <wolframteetz@gmail.com>
2015-07-23 23:11:46 +0200

    Update macdup.rb

--- macdup.rb
+++ macdup.rb
@@ -15,7 +15,8 @@ class Macdup < Formula
   end
 
   test do
-  system "chmod 0555 /usr/local/bin/macdup"
+  #system "chmod 0555 /usr/local/bin/macdup"
+      chmod 0555, Dir["#{bin}/macdup"]
     # Run the test with `brew test macdup`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
