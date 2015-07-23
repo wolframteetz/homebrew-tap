346b6aebdf2d2fe992e2525a080336612c550ef2
wolframteetz <wolframteetz@gmail.com>
2015-07-23 21:09:47 +0200

    Update macdup.rb

--- macdup.rb
+++ macdup.rb
@@ -5,9 +5,8 @@ class Macdup < Formula
   version "1.0.1"
   sha256 "98a4d08320b3142e5af33e8286272c3378b43c4f1780e0aba42187bca929bc5d"
 
-  # depends_on "cmake" => :build
-  depends_on :curl # if your formula requires any X11/XQuartz components
-  depends_on :jq
+  depends_on "curl"
+  depends_on "jq"
   
   def install
     bin.install "macdup"
@@ -15,9 +14,7 @@ class Macdup < Formula
 
   test do
     system "chmod 0555 /usr/local/bin/macdup"
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test macdup`. Options passed
+    # Run the test with `brew test macdup`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
