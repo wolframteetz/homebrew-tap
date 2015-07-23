21cbc51f7c5166b59ad8e4ecb4508410c660ea8e
wolframteetz <wolframteetz@gmail.com>
2015-07-24 00:30:58 +0200

    Update macdup.rb

--- macdup.rb
+++ macdup.rb
@@ -22,7 +22,7 @@ class Macdup < Formula
       #userServer = gets.chomp
       #inreplace "macdup", "userServer", userServer
       system "chmod ugo+w /usr/local/bin/macdup"
-      system "open /Applications/TextEdit.app '/usr/local/bin/macdup'"
+      system "open -b com.apple.TextEdit \'/usr/local/bin/macdup\'"
       system "chmod ugo-w /usr/local/bin/macdup"
       puts "Installation complete."
     # Run the test with `brew test macdup`. Options passed
