b6a02f1bdbc6f7ac765d62d2860d0158a0e2ad6d
Wolfram <wolframteetz@gmail.com>
2017-07-24 22:47:57 +0200

    Update fixpoint.rb

--- fixpoint.rb
+++ fixpoint.rb
@@ -1,8 +1,8 @@
 class Fixpoint < Formula
   desc "Fixpoint - Powerpoint video fix for iPhone/MOV videos recorded on OSX so they play nicely on windows"
   homepage "https://github.com/wolframteetz/fixpoint"
-  url "https://codeload.github.com/wolframteetz/FixPoint/zip/master"
-  version "1.0.2"
+  url "https://api.github.com/repos/wolframteetz/FixPoint/tarball/master"
+  version "1.0.3"
   sha256 "6f978b9ba1d80cbc66ebe6e0ce0d71d0c789132011c7525d6afe807e691ec62c"
 
   depends_on "ffmpeg"
