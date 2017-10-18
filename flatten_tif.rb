97ba7009612fcb4c11e63ea9c2bd811d4176f587
Wolfram <wolframteetz@gmail.com>
2017-10-19 00:14:06 +0200

    Update flatten_tif.rb

--- flatten_tif.rb
+++ flatten_tif.rb
@@ -2,7 +2,7 @@ class FlattenTif < Formula
   desc "Flatten_tif - Flatten TIFF files on OSX"
   homepage "https://github.com/wolframteetz/ocrtools"
   url "https://api.github.com/repos/wolframteetz/ocrtools/tarball/master"
-  version "1.0.5"
+  version "1.0.6"
   sha256 "fec3e7379feaa633379ef450c18299ab100d4b3713e5acd580086592906905a0"
 
   depends_on "imagemagick"
