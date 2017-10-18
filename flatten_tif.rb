4eccb29885bbcc69144cfaa74d9915ac72febb2d
Wolfram <wolframteetz@gmail.com>
2017-10-18 23:48:52 +0200

    Update flatten_tif.rb

--- flatten_tif.rb
+++ flatten_tif.rb
@@ -17,7 +17,7 @@ class FlattenTif < Formula
 
   test do
     ENV.deparallelize
-      chmod ugo+x, Dir["/usr/local/bin/flatten_all_tifs"]
+      chmod 0555, Dir["/usr/local/bin/flatten_all_tifs"]
       puts "Installation complete, run with e.g. bash> flatten_all_tifs *.tif"
   end
 end
